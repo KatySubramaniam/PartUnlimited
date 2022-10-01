edc9d783c2f39f93ddeffb8815539b707282eae3
KatySubramaniam <113910062+KatySubramaniam@users.noreply.github.com>
2022-10-02 00:23:37 +0800

    Second and third commit azure

--- PartUnlimited/PartsUnlimited/PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartUnlimited/PartsUnlimited/PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {   // My First Changes by Katy Subramaniam
-
+        // My second change.
         [Key]
         public int CartItemId { get; set; }
         [Required]
